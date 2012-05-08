74ada287a2f8a6090cc1a14c17f2fd9ea3d7811b
mhutter <manuel.hutter@gmail.com>
2012-05-08 22:13:16 +0200

    added groups

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,12 +1,18 @@
 package "git-core"
 
-group "admin"
+group node[:user][:name]
 user node[:user][:name] do
   password node[:user][:password]
-  gid "admin"
+  gid node[:user][:name]
   home "/home/#{node[:user][:name]}"
   supports manage_home: true
   shell "/bin/bash"
+end
+
+group "admin" do
+  gid 999
+  members [node[:user][:name]]
+  append true
 end
 
 template "/home/#{node[:user][:name]}/.bashrc" do
