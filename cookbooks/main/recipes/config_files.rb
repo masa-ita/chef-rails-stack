d7837508a0ab2f297e2c35e70d5d6ad16af045d8
Manuel Hutter <manuel.hutter@gmail.com>
2012-05-10 15:20:37 +0200

    made user creation optional, added package postgresql-9.1

--- cookbooks/main/recipes/config_files.rb
+++ cookbooks/main/recipes/config_files.rb
@@ -9,16 +9,33 @@
 end
 
 # user specific templates
-%w{bashrc profile screenrc gitconfig}.each do |f|
-  template "/home/#{node[:user][:name]}/.#{f}" do
-    source "#{f}-user.erb"
-    owner node[:user][:name]
+if node[:user]
+  %w{bashrc profile screenrc gitconfig}.each do |f|
+    template "/home/#{node[:user][:name]}/.#{f}" do
+      source "#{f}-user.erb"
+      owner node[:user][:name]
+    end
   end
-end
 
-file "/home/#{node[:user][:name]}/.gemrc" do
-  owner node[:user][:name]
-  content "gem: --no-rdoc --no-ri\n"
+  file "/home/#{node[:user][:name]}/.gemrc" do
+    owner node[:user][:name]
+    content "gem: --no-rdoc --no-ri\n"
+  end
+  
+  # for security..
+  directory "/home/#{node[:user][:name]}/.ssh" do
+    mode "700"
+    owner node[:user][:name]
+    group node[:user][:name]
+  end
+
+  if node[:user][:authorized_keys]
+    file "/home/#{node[:user][:name]}/.ssh/authorized_keys" do
+      owner node[:user][:name]
+      mode "600"
+      content node[:user][:authorized_keys]  
+    end
+  end
 end
 
 # templates for root
@@ -29,20 +46,3 @@ template "/root/.bashrc" do
   mode "644"
 end
 
-
-# for security..
-directory "/home/#{node[:user][:name]}/.ssh" do
-  mode "700"
-  owner node[:user][:name]
-  group node[:user][:name]
-end
-
-# optional stuff below here
-
-if node[:user][:authorized_keys]
-  file "/home/#{node[:user][:name]}/.ssh/authorized_keys" do
-    owner node[:user][:name]
-    mode "600"
-    content node[:user][:authorized_keys]  
-  end
-end
--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -3,24 +3,26 @@ package "git-core"
 package "screen"
 
 # users and groups
-group node[:user][:name]
-user node[:user][:name] do
-  password node[:user][:password]
-  gid node[:user][:name]
-  home "/home/#{node[:user][:name]}"
-  supports manage_home: true
-  shell "/bin/bash"
-end
+if node[:user]
+  group node[:user][:name]
+  user node[:user][:name] do
+    password node[:user][:password]
+    gid node[:user][:name]
+    home "/home/#{node[:user][:name]}"
+    supports manage_home: true
+    shell "/bin/bash"
+  end
 
-group "admin" do
-  gid 999
-  members [node[:user][:name]]
-  append true
+  group "admin" do
+    gid 999
+    members [node[:user][:name]]
+    append true
+  end
 end
 
 include_recipe "main::config_files"
-
 include_recipe "nginx::source"
+package "postgresql-9.1"
 
 # TODO: postgresql
 # does currently not work because of broken postgresql::server
