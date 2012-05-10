9723b771cf564102ef52b5bd7836e95ceea21925
Manuel Hutter <manuel.hutter@gmail.com>
2012-05-10 16:37:05 +0200

    added default attributes

---
+++ cookbooks/main/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:environment]     = "production"
+default[:user][:name]     = "deployer"
+default[:rbenv][:plugins] = ['https://github.com/sstephenson/ruby-build.git', 'https://github.com/jamis/rbenv-gemset.git']
+
